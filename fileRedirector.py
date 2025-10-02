3fbcb9c6f54f590d04f4531065827abe9da02fe3
Filaxis <filaxis@gmail.com>
2025-10-02 14:28:07 +0200

    Add files via upload
    Setting up the paths to "clientDatabase" and "clientFilesTransport" as arguments to the fileRedirector() function at the bottom of the code is required to run the program.
    
    Set the paths as follows:
    
    1) set "clientFoldersPath" as the path to the folder "clientDatabase" on your PC
    
    2) set "clientFilesPath" as the path to the folder "clientFilesTransport" on your PC
    
    # you can download both "clientDatabase" and "clientFilesTransport" from this repository
    
    # keep in mind the correct order of the arguments: fileRedirector(clientFoldersPath,clientFilesPath)

---
+++ fileRedirector.py
@@ -0,0 +1,48 @@
+# This simple code is created to automate the boring
+# task of moving files from Transport folder to the
+# respective client folders based on matching the
+# the files to be moved with the respective
+# subfolders based on the client's surname and name.
+
+import os, shutil, re
+
+def getFolderNamelist(clientFoldersPath):
+    clientFolderList = [entry for entry in os.listdir(clientFoldersPath) if os.path.isdir(os.path.join(clientFoldersPath, entry))]
+    #print(clientFolderList)
+    return clientFolderList
+
+def getFileNamelist(clientFilespath):
+    clientFileList = [entry for entry in os.listdir(clientFilespath) if os.path.isfile(os.path.join(clientFilespath, entry))]
+    #print(clientFileList)
+    return clientFileList
+
+def fileRedirector(clientFoldersPath, clientFilespath):
+    folderList = getFolderNamelist(clientFoldersPath)
+    fileList = getFileNamelist(clientFilespath)
+    clientNameList = ['The list of all clients for whom at least one file was moved']
+    file = open(clientFoldersPath + '\\\\clientFileList.txt', 'w') 
+    print("The list of all clients for whom at least one file was moved : ", file.name)
+    file.close()
+    for Folder in folderList:
+        folderName = re.compile(Folder)
+        for fileName in fileList:
+            if folderName.search(fileName):
+                shutil.copy(clientFilespath + '\\\\' + fileName, clientFoldersPath + '\\\\' + Folder)
+                if (Folder in clientNameList) == False:
+                    clientNameList.append(Folder)
+    print(clientNameList)
+
+    # write the file header including the current date
+    with open(clientFoldersPath + '\\\\clientFileList.txt', "a") as file:
+        for Client in clientNameList:
+            file.write(Client)
+            file.write("\n")
+
+
+
+#getFolderNamelist('C:\\Users\\filax\OneDrive\\Desktop\\Code-repository\\2025-08-03-Investcon-file-redirector\\clientDatabase')
+
+#getFileNamelist('C:\\Users\\filax\\OneDrive\\Desktop\\Code-repository\\2025-08-03-Investcon-file-redirector\\clientFilesTransport')
+
+fileRedirector('C:\\Users\\filax\OneDrive\\Desktop\\Code-repository\\2025-08-03-Investcon-file-redirector\\clientDatabase',
+               'C:\\Users\\filax\\OneDrive\\Desktop\\Code-repository\\2025-08-03-Investcon-file-redirector\\clientFilesTransport')
